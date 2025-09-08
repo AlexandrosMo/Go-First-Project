9a57f49c40bc96aaa3866f6ed685f0f5f084ae88
Alexandros Moraitis <115009236+AlexandrosMo@users.noreply.github.com>
2025-09-08 18:44:02 +0300

    added a confirmation message

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -13,6 +13,8 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v  are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var bookings []string
+
 	var firstName string
 	var lastName string
 	var email string
@@ -34,7 +36,16 @@ func main() {
 
 	remainingTickets = remainingTickets - userTickets
 
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("the whole Slice: %v\n", bookings)
+	fmt.Printf("the first value: %v\n", bookings[0])
+	fmt.Printf("Slice type: %v\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 
 }
