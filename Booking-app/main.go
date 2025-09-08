58c6b8280ebf1846f70b631d7dada9cbe1df4910
Alexandros Moraitis <115009236+AlexandrosMo@users.noreply.github.com>
2025-09-08 18:56:35 +0300

    added a if loop and break after the purchase

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -6,6 +6,7 @@ func main() {
 	var conferenceName string = "Go conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	var bookings []string
 
 	fmt.Printf("conferenceTickets is %T,remainingTickets is %T,conferenceName is %T \n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -13,39 +14,48 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v  are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var bookings []string
+	for {
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	// ask user for their name
+		// ask user for their name
 
-	fmt.Println("Enter your name:")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter your name:")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name:")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email:")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email:")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter number of tickets:")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter number of tickets:")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
+		remainingTickets = remainingTickets - userTickets
 
-	bookings = append(bookings, firstName+" "+lastName)
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("the whole Slice: %v\n", bookings)
-	fmt.Printf("the first value: %v\n", bookings[0])
-	fmt.Printf("Slice type: %v\n", bookings)
-	fmt.Printf("Slice length: %v\n", len(bookings))
+		if userTickets <= 0 {
+			fmt.Println("Please enter a valid number of tickets")
+		}
+		if userTickets > 0 {
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+			fmt.Printf("the whole Slice: %v\n", bookings)
+			fmt.Printf("the first value: %v\n", bookings[0])
+			fmt.Printf("Slice type: %v\n", bookings)
+			fmt.Printf("Slice length: %v\n", len(bookings))
 
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
+			fmt.Printf("These are all our bookings: %v\n", bookings)
+
+			break
+		}
+
+	}
 }
